d10122b853fc8a8308c24d6ad5f826b48c4130b0
wahlnicolai <wahl.nicolai@gmail.com>
2022-10-25 18:10:52 +0200

    Begynner også litt på oppgave 6

--- src/main/java/no/oslomet/cs/algdat/Oblig3/SBinTre.java
+++ src/main/java/no/oslomet/cs/algdat/Oblig3/SBinTre.java
@@ -158,7 +158,10 @@ public class SBinTre<T> {
 
     //Oppgave 3
     private static <T> Node<T> førstePostorden(Node<T> p) {
-        throw new UnsupportedOperationException("Ikke kodet ennå!");
+        //Prøver eksempel fra boken, vet ikke om det er relevant da :)
+
+
+        return p;
     }
 
     private static <T> Node<T> nestePostorden(Node<T> p) {
@@ -166,19 +169,7 @@ public class SBinTre<T> {
     }
 
 
-    public boolean fjern(T verdi) {
-        throw new UnsupportedOperationException("Ikke kodet ennå!");
-    }
 
-    public int fjernAlle(T verdi) {
-        throw new UnsupportedOperationException("Ikke kodet ennå!");
-    }
-
-
-
-    public void nullstill() {
-        throw new UnsupportedOperationException("Ikke kodet ennå!");
-    }
 
 
 
@@ -200,6 +191,57 @@ public class SBinTre<T> {
 
     static <K> SBinTre<K> deserialize(ArrayList<K> data, Comparator<? super K> c) {
         throw new UnsupportedOperationException("Ikke kodet ennå!");
+    }
+    //Oppgave 6
+    public boolean fjern(T verdi) { //Bruker kode fra kompendiet programkode 5.2.8d
+
+        if (verdi == null) return false;  // treet har ingen nullverdier
+
+        Node<T> p = rot, q = null;   // q skal være forelder til p
+
+        while (p != null)            // leter etter verdi
+        {
+            int cmp = comp.compare(verdi,p.verdi);      // sammenligner
+            if (cmp < 0) { q = p; p = p.venstre; }      // går til venstre
+            else if (cmp > 0) { q = p; p = p.høyre; }   // går til høyre
+            else break;    // den søkte verdien ligger i p
+        }
+        if (p == null) return false;   // finner ikke verdi
+
+        if (p.venstre == null || p.høyre == null)  // Tilfelle 1) og 2)
+        {
+            Node<T> b = p.venstre != null ? p.venstre : p.høyre;  // b for barn
+            if (p == rot) rot = b;
+            else if (p == q.venstre) q.venstre = b;
+            else q.høyre = b;
+        }
+        else  // Tilfelle 3)
+        {
+            Node<T> s = p, r = p.høyre;   // finner neste i inorden
+            while (r.venstre != null)
+            {
+                s = r;    // s er forelder til r
+                r = r.venstre;
+            }
+
+            p.verdi = r.verdi;   // kopierer verdien i r til p
+
+            if (s != p) s.venstre = r.høyre;
+            else s.høyre = r.høyre;
+        }
+
+        antall--;   // det er nå én node mindre i treet
+        return true;
+    }
+
+    public int fjernAlle(T verdi) {
+        return 0;
+    }
+
+
+
+    public void nullstill() {
+
     }
 
 
